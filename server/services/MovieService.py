6009fda2981f87e449431acadb12c92d22e2d57e
AntDeivid <deividsantos121@gmail.com>
2025-02-03 21:30:16 -0300

    search movies method

---
+++ server/services/MovieService.py
@@ -0,0 +1,18 @@
+from typing import List, Optional
+
+from common.models.movie import Movie
+from common.models.streaming_option import StreamingOption
+from server.services.TMDbService import TMDbService
+
+
+class MovieService:
+    @classmethod
+    async def search_movie(cls, query: str, year: Optional[int] = None) -> List[Movie]:
+        movies = await TMDbService.search_movie(query, year)
+        if not movies:
+            raise ValueError("No movies found")
+        return movies
+
+    @classmethod
+    async def get_streaming_options(cls, movie_id: str) -> List[StreamingOption]:
+        pass
